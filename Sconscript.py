5796c5e98bf30f79916ed88ece7c767601ab29f0
Jonathan Lara <jonabimael@gmail.com>
2022-04-08 10:41:01 -0500

    Configurando Sconscript

--- Sconscript.py
+++ Sconscript.py
@@ -1,9 +1,8 @@
-print('[Mensaje] Generando libreria estatica latino-regex')
+print('[Mensaje] Generando libreria compartida latino-regex')
 
 import platform
 sistema = platform.system()
 
 Import('entorno','ruta_librerias')
 
-entorno.StaticLibrary(target=ruta_librerias+"regex",source="src/regex.c")
-#entorno.SharedLibrary(target=ruta_librerias+"regex2",source="src/regex.c")
+entorno.SharedLibrary(target=ruta_librerias+"regex2",source="src/regex.c")
